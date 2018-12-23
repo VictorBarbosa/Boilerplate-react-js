a15a2b5886e6404ef092cfa84182ea6cd9ead5b1
victorbarbosa <victor.hugo.p.barbosa@hotmail.com>
2018-12-22 21:14:59 -0500

    =creating a Boilerplate to react js

---
+++ README.md
@@ -0,0 +1,2 @@
+"# Boilerplate-react-js" 
+ 
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React ,{Component}from 'react';
+class ComponentName extends Component {
+  render() {
+    return (
+      <div>The component is here</div>
+    );
+  }
+}
+export default ComponentName;
